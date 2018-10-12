b98b99f1cc563eceb22bd4342f42540141d83ffc
Matt Watier <mattwatier@gmail.com>
2018-10-12 09:08:21 -0400

    Build to make git hub working

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   outputDir: 'docs',
-  assetsDir: 'assets'
+  assetsDir: 'assets',
+  baseUrl: '/CSTool/',
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }
