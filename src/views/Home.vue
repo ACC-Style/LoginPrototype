729dcb67679f83522e6d2857c603806a1f0b7c57
Matt Watier <mattwatier@gmail.com>
2018-10-12 10:22:25 -0400

    login button disable error transitions
    Error Transistion and disabled

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,16 @@
 	<div class="home texture_light padding_4" style="height:100vh;">
 		<div class="grid-x">
 			<div class="cell small-10 medium-8 large-6 margin_auto">
+				<div class="card shadow_n2 back_alert_2">
+					<div class="card-divider back_alert texture_dust color_white font_3">
+						Error Loging In. 
+					</div>
+					<div class="card-section font_1"> <p>Your email and password does not match our records.</p></div>
+				</div>
+			</div>
+		</div>
+		<div class="grid-x">
+			<div class="cell small-10 medium-8 large-6 margin_auto">
 				<div class="card shadow_2 cell  ">
 					<div class="card-divider color_white font-size_3 padding_4 texture_dust  back_primary">
 						ACC Staff Login
@@ -10,10 +20,10 @@
 						<form class="grid-container">
 						    <div class="grid-x grid-padding-x">
 						        <div class="medium-6 cell">
-						                <label>Name  <input type="text" required></label>
+						                <label>Username  <input type="text" v-model="username" placeholder="youremail@acc.org" required></label>
 						        </div>
 						        <div class="medium-6 cell">
-						                <label>Email  <input type="password" required></label>
+						                <label>Email  <input type="password" v-model="password" placeholder="password" required></label>
 						            
 						        </div>
 						    </div>
@@ -21,7 +31,7 @@
 						<div class="grid-container">
 							<div class="grid-x justify-end">
 								<div class="medium-4 cell ">
-<router-link to="/search"  class="button disabled display-block color_white margin-bottom_0 border_radius expanded">Log In <i class="fal fa-arrow-alt-right"></i></router-link>
+										<router-link to="/search" v-bind:class="{disabled: !submitDisabled()}" class="button display-block color_white margin-bottom_0 border_radius expanded">Log In <i class="fal fa-arrow-alt-right"></i></router-link>
 									</div>
 							</div>
 						</div>
@@ -38,7 +48,22 @@
 <script>
 // @ is an alias to /src
 export default {
-  name: "home"
+  name: "home",
+  data() {
+    return {
+    	username:'',
+    	password:''
+
+    };
+	},
+	methods:{
+		submitDisabled:function(){
+			if(this.username != "" && this.password != "" ){
+				return true;
+			}
+			return false;
+		}
+	}
 };
 </script>
 <style>
